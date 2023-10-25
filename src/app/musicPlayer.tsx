c874523790198c9fe2b58a418268318d6f6f36c5
Clark <mcevoyclark@gmail.com>
2023-10-25 18:17:01 +0200

    pause button implementation

--- src/app/musicPlayer.tsx
+++ src/app/musicPlayer.tsx
@@ -4,10 +4,13 @@ import React, { useEffect, useRef, useState } from "react";
 import { PlayIconOutline, PlayIconSolid } from "./components/icons/PlayIcon";
 import { PauseIconOutline, PauseIconSolid } from "./components/icons/PauseIcon";
 import CustomButton from "./components/Card/CustomButton";
+import { StopIconOutline, StopIconSolid } from "./components/icons/StopIcon";
 
 function MusicPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+
+  const iconStyle = "w-10 h-10";
 
   useEffect(() => {
     setAudio(new Audio("/static/starry-night (1).mp3"));
@@ -19,6 +22,7 @@ function MusicPlayer() {
   const audioRef = useRef<HTMLAudioElement>(song);
 
   function toggleAudio(): void {
+    console.log(audioRef)
     if (isPlaying) {
       audioRef.current?.pause();
     } else {
@@ -29,21 +33,27 @@ function MusicPlayer() {
 
   return (
     <div>
-      <button className="" onClick={toggleAudio}>
+      <div>
         {isPlaying ? (
           <CustomButton
             onClick={toggleAudio}
-            solid={<PlayIconSolid className="w-10-10" />}
-            outline={<PlayIconOutline className="w-10 h-10" />}
+            solid={<PauseIconSolid className={iconStyle} />}
+            outline={<PauseIconSolid className={iconStyle} />}
           />
         ) : (
           <CustomButton
             onClick={toggleAudio}
-            solid={<PauseIconSolid className="w-10-10" />}
-            outline={<PauseIconOutline className="w-10 h-10" />}
+            solid={<PlayIconSolid className={iconStyle} />}
+            outline={<PlayIconOutline className={iconStyle} />}
           />
         )}
-      </button>
+        {/* Stop button */}
+        <CustomButton
+          onClick={toggleAudio}
+          solid={<StopIconSolid className={iconStyle} />}
+          outline={<StopIconOutline className={iconStyle} />}
+        />
+      </div>
     </div>
   );
 }
