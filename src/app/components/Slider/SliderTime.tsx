c6af5d27ac9542f533fbaa3519a87928251e3d0b
Clark <mcevoyclark@gmail.com>
2023-11-17 16:57:20 +0100

    work in progress

--- src/app/components/Slider/SliderTime.tsx
+++ src/app/components/Slider/SliderTime.tsx
@@ -1,58 +1,61 @@
 import React, { useEffect, useState } from "react";
-import './sliderCss.css'
+import "./sliderCss.css";
 import { secondsToHms } from "@/app/utils";
 
 interface Props {
-    audioRef: any
+  audioRef: any;
 }
 
 function SliderTime({ audioRef }: Props) {
-    const [currentTime, setCurrentTime] = useState(0)
-    const [duration, setDuration] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
 
-    function roundUp(n : number) {
-      return Math.ceil(n);
-    }
+  function roundUp(n: number) {
+    return Math.ceil(n);
+  }
 
-    useEffect(() => {
-        const audio = audioRef.current;
-    
-        const updateTime = () => {
-          setCurrentTime(roundUp(audio.currentTime));
-        };
-    
-        const updateDuration = () => {
-          setDuration(roundUp(audio.duration));
-        };
-    
-        audio.addEventListener('timeupdate', updateTime);
-        audio.addEventListener('loadedmetadata', updateDuration);
-    
-        return () => {
-          audio.removeEventListener('timeupdate', updateTime);
-          audio.removeEventListener('loadedmetadata', updateDuration);
-        };
-      },);
+  useEffect(() => {
+    const audio = audioRef.current;
 
-      const handleTimeChange = (event: any) => {
-        const audio = audioRef.current;
-        audio.currentTime = event.target.value;
-        setCurrentTime(audio.currentTime);
-      };
-    
+    const updateTime = () => {
+      setCurrentTime(roundUp(audio.currentTime));
+    };
+
+    const updateDuration = () => {
+      setDuration(roundUp(audio.duration));
+    };
+
+    audio.addEventListener("timeupdate", updateTime);
+    audio.addEventListener("loadedmetadata", updateDuration);
+
+    return () => {
+      audio.removeEventListener("timeupdate", updateTime);
+      audio.removeEventListener("loadedmetadata", updateDuration);
+    };
+  });
+
+  const handleTimeChange = (event: any) => {
+    const audio = audioRef.current;
+    audio.currentTime = event.target.value;
+    setCurrentTime(audio.currentTime);
+  };
 
   return (
-    <div className="flex items-center px-6">
-      <p className="px-4 text-ternary">{secondsToHms(currentTime)}</p>
-      <input
-        type="range"
-        min={0}
-        max={duration}
-        value={currentTime}
-        onChange={handleTimeChange}
-        className="cursor-pointer time-slider"
+    <div className="flex items-center px-6 space-x-2">
+      <p className="px-4 text-ternary bg-orange-500 w-8">{secondsToHms(currentTime)}</p>
+      <div>
+        <input
+          type="range"
+          min={0}
+          max={duration}
+          value={currentTime}
+          onChange={handleTimeChange}
+          className="cursor-pointer time-slider w-44"
         />
-        <p className="px-4 text-ternary">{secondsToHms(duration-currentTime)}</p>
+      </div>
+      <p className="px-4 w-8 text-ternary">
+        {secondsToHms(duration - currentTime)}
+      </p>
     </div>
   );
 }
