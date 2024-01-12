985dfe0eec25512f2eaf0b51c357ddb789def53c
Clark <mcevoyclark@gmail.com>
2024-01-12 16:24:34 +0100

    fix: minor change

--- src/app/components/Selection/index.tsx
+++ src/app/components/Selection/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { songs } from "../../../../data/dummyData";
-import SongRow from "./Row";
+import React from 'react';
+import { songs } from '../../../../data/dummyData';
+import SongRow from './Row';
 
 interface Props {
   selection: number;
@@ -11,7 +11,13 @@ function Selection({ selection, toggleSelection }: Props) {
   return (
     <div className="w-full overflow-auto overflow-y-auto h-full">
       {songs.map((song: any, i: any) => (
-        <SongRow id={i} isActive={selection === i} toggleSelection={toggleSelection} key={i} song={song} />
+        <SongRow
+          id={i}
+          isActive={selection === i}
+          toggleSelection={toggleSelection}
+          key={i}
+          song={song}
+        />
       ))}
     </div>
   );
